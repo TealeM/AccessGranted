ed3e86434fa77ce706b3f813e2204a3b9614b99b
TEALE-ASUS\teale <tealemasrani@gmail.com>
2020-11-10 20:39:55 -0700

    About Us page created. Added some superficial stuff to the menu and home page.

--- lib/helper/drawer.dart
+++ lib/helper/drawer.dart
@@ -2,12 +2,15 @@ import 'package:access_granted/helper/authenticate.dart';
 import 'package:access_granted/helper/constants.dart';
 import 'package:access_granted/helper/helperfunctions.dart';
 import 'package:access_granted/services/auth.dart';
+import 'package:access_granted/views/aboutUs.dart';
 import 'package:access_granted/views/search.dart';
 import 'package:access_granted/views/chatRoomsScreen.dart';
 import 'package:access_granted/views/userProfile.dart';
 import 'package:access_granted/views/homeScreen.dart';
 import 'package:access_granted/widgets/widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MyDrawer extends StatelessWidget {
   @override
@@ -21,7 +24,7 @@ class MyDrawer extends StatelessWidget {
               padding: EdgeInsets.zero,
               children: [
                 ListTile(
-                  title: Text('Home'),
+                  title: Text('Home', style: TextStyle(fontSize: 24, color: Color(0xFF1EA699))),
                   onTap: () {
                     Navigator.pushReplacement(
                         context,
@@ -30,7 +33,7 @@ class MyDrawer extends StatelessWidget {
                   },
                 ),
                 ListTile(
-                  title: Text('My Profile'),
+                  title: Text('My Profile', style: TextStyle(fontSize: 24, color: Color(0xFF1EA699))),
                   onTap: () {
                     Navigator.pushReplacement(
                         context,
@@ -39,7 +42,7 @@ class MyDrawer extends StatelessWidget {
                   },
                 ),
                 ListTile(
-                  title: Text('Postings'),
+                  title: Text('Search Users', style: TextStyle(fontSize: 24, color: Color(0xFF1EA699))),
                   onTap: () {
                     Navigator.pushReplacement(
                         context,
@@ -49,7 +52,7 @@ class MyDrawer extends StatelessWidget {
                   },
                 ),
                 ListTile(
-                  title: Text('Messages'),
+                  title: Text('Messages', style: TextStyle(fontSize: 24, color: Color(0xFF1EA699))),
                   onTap: () {
                     Navigator.pushReplacement(
                         context,
@@ -59,8 +62,13 @@ class MyDrawer extends StatelessWidget {
                   },
                 ),
                 ListTile(
-                  title: Text('About Us'),
+                  title: Text('About Us', style: TextStyle(fontSize: 24, color: Color(0xFF1EA699))),
                   onTap: () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AboutUs())
+                    );
 
                   },
                 ),
--- lib/views/homeScreen.dart
+++ lib/views/homeScreen.dart
@@ -19,38 +19,65 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBarMain(context),
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height-200,
-          alignment: Alignment.center,
-          child: Container(
-            height: 400,
-            child: GridView.count(
-              scrollDirection: Axis.vertical,
-              crossAxisCount: 2 ,
-              children: List.generate(4,(index){
-                return Container(
-                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-                  child: Container(
-                    padding: EdgeInsets.symmetric(vertical: 55),
-                    color: Colors.amber,
-                    child: Text ('Accessible Gaming News!',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold
-                          ),
-                      textAlign: TextAlign.center,
-                    ),
-                  )
-                );
-              }),
-            ),
-          ),
-        ),
+      appBar: AppBar(
+        title: Text("Welcome to Access Granted!"),
       ),
-      drawer: MyDrawer ()
-    );
+      body: SingleChildScrollView(
+        child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 5),
+            height: MediaQuery.of(context).size.height-380,
+            alignment: Alignment.center,
+              child: Scrollbar(
+                child: GridView.count(
+                  scrollDirection: Axis.vertical,
+                  crossAxisCount: 2 ,
+                  children: List.generate(6,(index){
+                    return Container(
+                      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                      child: Container(
+                        padding: EdgeInsets.symmetric(vertical: 55),
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),  color: Colors.amber,),
+                        child: Text ('Accessible Gaming News!',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold
+                              ),
+                          textAlign: TextAlign.center,
+                        ),
+                      )
+                    );
+                  }),
+                ),
+          ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 50),
+            child: Column(
+              children: [
+                Container(
+                  color: Colors.blueGrey,
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: Text('We should celebrate neurodiversity. The world would be poorer and life duller if we were all the same.',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontStyle: FontStyle.italic
+                  )
+              )),
+                Container(
+                  alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: Text ('- Neil Milliken',
+                  style: TextStyle(color: Colors.white, fontSize: 24)
+                ))
+            ]),
+          ),
+        ]),
+      ),
+        drawer: MyDrawer ()
+      );
   }
 }
